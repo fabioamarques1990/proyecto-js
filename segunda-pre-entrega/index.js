58771decd8d764df1e28b98e277fe5302247ac6c
Fabio Marques <fabio.amarques1990@gmail.com>
2022-07-20 21:52:40 -0500

    Finalizado el desafio complementario 3.

--- segunda-pre-entrega/index.js
+++ segunda-pre-entrega/index.js
@@ -1,16 +1,16 @@
 // paquetes
 const paquetes = [
-    {Nombre: "1", Descripción: "Guía, tabla y paddle", Precio: 150},
-    {Nombre: "2", Descripción: "Guía, tabla, paddle y chaleco", Precio: 200},
-    {Nombre: "3", Descripción: "Guía, tabla, paddle, chaleco y cámara", Precio: 300},
-    {Nombre: "4", Descripción: "Guía, tabla, paddle, chaleco, cámara y drone", Precio: 500},
+    { Nombre: "1", Descripción: "Guía, tabla y paddle", Precio: 150 },
+    { Nombre: "2", Descripción: "Guía, tabla, paddle y chaleco", Precio: 200 },
+    { Nombre: "3", Descripción: "Guía, tabla, paddle, chaleco y cámara", Precio: 300 },
+    { Nombre: "4", Descripción: "Guía, tabla, paddle, chaleco, cámara y drone", Precio: 500 },
 ];
 
 
 
 
 
-// Iniciar sesión
+/* // Iniciar sesión
 let usuario 
 let usuarioEnLS = (localStorage.getItem("usuario"));
 console.log(usuario);
@@ -27,7 +27,7 @@ if (usuarioEnLS) {
     usuario = document.getElementById ("usuario", usuario.value);
     localStorage.setItem("usuario", usuario);
     let button = document.getElementById("btnIngresar")
-    button.onclick = () => {alert ("Bienvenido")}
+    button.onclick = () => {alert ("Bienvenido")} */
 
 
 
@@ -35,23 +35,23 @@ if (usuarioEnLS) {
 
 
 let formulario = document.getElementById("formulario");
-formulario.addEventListener ("submit", validarFormulario);
+formulario.addEventListener("submit", validarFormulario);
 
-function validarFormulario (e){
+function validarFormulario(e) {
     e.preventDefault();
     let formulario = e.target
 
     let cuestionario = []
 
     // Usando constructor
-    let nombre = formulario.children[0].value.toLowerCase();
-    let edad = formulario.children[1].value.toLowerCase();
-    let numero = formulario.children[2].value.toLowerCase();
-    let experiencia = formulario.children[3].value.toLowerCase();
-    let ninos = formulario.children[4].value.toLowerCase();
-    let paquete = formulario.children[5].value.toLowerCase();
+    let nombre = document.getElementById("nombre");
+    let edad = document.getElementById("edad");
+    let numero = document.getElementById("numero");
+    let experiencia = document.getElementById("experiencia");
+    let ninos = document.getElementById("ninos");
+    let paquete = document.getElementById("paquete");
 
-    function Cliente (nombre, edad, numero, experiencia, ninos) {
+    function Cliente(nombre, edad, numero, experiencia, ninos, paquete) {
         this.nombre = nombre
         this.edad = edad
         this.numero = numero
@@ -59,25 +59,32 @@ function validarFormulario (e){
         this.ninos = ninos
         this.paquete = paquete
     };
-    
-    const cliente1 = new Cliente (nombre, edad, numero, experiencia, ninos);
+
+    const cliente1 = new Cliente(nombre.value, edad.value, numero.value, experiencia.value, ninos.value, paquete.value);
     cuestionario.push(cliente1);
     console.log(cuestionario);
 
-let button2 = document.getElementById("btnPrincipal")
-button2.onclick = () => {alert ("Listo, tu información está guardada.")}
+    // Usando funcion de orden superior
+    let precioPaquete = paquetes.find(elemento => elemento.Nombre === paquete.value);
+    console.log(precioPaquete);
+    let precio = 100;
+    let result = numero.value * precio + precioPaquete.Precio;
 
-// Usando ciclo
-while (numero > 8) {
-    alert ("Lo sentimos, no pueden ser más de 8 personas a la vez.")
-    break;
+    const totalReserva = () => {
+        contenedor.innerHTML = `<div>
+                                <p>¡Hola ${nombre.value}!
+                                El total de tu reserva es de ${result}.</p>
+                            </div>`
+    };
+
+    const validarCampos = () => {
+        let validacion = (nombre.value == "" || edad.value == "" || numero.value == "" || experiencia.value == "" || ninos.value == "" || paquete.value == "") ? false : true;
+        return validacion
+    };
+
+    let button = document.getElementById("btnPrincipal")
+    button.onclick = () => {
+        validarCampos() ? totalReserva() : alert("Debe completar todos los campos");
+    };
+
 }
-
-// Usando funcion de orden superior
-let precioPaquete = paquetes.find(elemento => elemento.Nombre === paquete);
-let precio = 100;
-let result = numero * precio + precioPaquete.Precio;
-
-alert ("El total de tu reserva es " + result);
-
-}}
